a3ae270279783cfb88b6c8227ef1baaa8c6dde74
Harsh <imharshalashi333@gmail.com>
2022-09-11 14:15:28 +0530

    JAVASCRIPT was added

--- script.js
+++ script.js
@@ -13,3 +13,110 @@ if (close) {
     nav.classList.remove("active");
   });
 }
+
+console.log("running");
+
+let carts = document.querySelectorAll(".add-cart");
+
+let products = [
+  {
+    name: "Cartoon Astronaut T-Shirt",
+    tag: "cat-1",
+    price: 30,
+    inCart: 0,
+  },
+  {
+    name: "Cartoon Astronaut T-Shirt",
+    tag: "cat-2",
+    price: 12,
+    inCart: 0,
+  },
+  {
+    name: "Cartoon Astronaut T-Shirt",
+    tag: "cat-3",
+    price: 18,
+    inCart: 0,
+  },
+  {
+    name: "Cartoon Astronaut T-Shirt",
+    tag: "cat-4",
+    price: 15,
+    inCart: 0,
+  },
+  {
+    name: "Cartoon Astronaut T-Shirt",
+    tag: "cat-5",
+    price: 60,
+    inCart: 0,
+  },
+  {
+    name: "Cartoon Astronaut T-Shirt",
+    tag: "cat-6",
+    price: 40,
+    inCart: 0,
+  },
+  {
+    name: "Cartoon Astronaut T-Shirt",
+    tag: "cat-7",
+    price: 10,
+    inCart: 0,
+  },
+  {
+    name: "Cartoon Astronaut T-Shirt",
+    tag: "cat-8",
+    price: 20,
+    inCart: 0,
+  },
+];
+
+for (let i = 0; i < carts.length; i++) {
+  carts[i].addEventListener("click", () => {
+    cartNumbers(products[i]);
+  });
+}
+
+function onLoadCartNumbers() {
+  let productNumbers = localStorage.getItem("cartNumbers");
+  if (productNumbers) {
+    document.querySelector(".cart span").textContent = productNumbers;
+  }
+}
+
+function cartNumbers(product) {
+  let productNumbers = localStorage.getItem("cartNumbers");
+  productNumbers = parseInt(productNumbers);
+  if (productNumbers) {
+    localStorage.setItem("cartNumbers", productNumbers + 1);
+    document.querySelector(".cart span").textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem("cartNumbers", 1);
+    document.querySelector(".cart span").textContent = 1;
+  }
+
+  setItems(product);
+}
+
+function setItems(product) {
+  let cartItems = localStorage.getItem("productsInCart");
+  cartItems = JSON.parse(cartItems);
+  console.log("My Cart Items Are", cartItems);
+
+  if (cartItems != null) {
+    if (cartItems[products.tag] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.tag]: product,
+      };
+    }
+    cartItems[product.tag].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]: product,
+    };
+  }
+
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+onLoadCartNumbers();
